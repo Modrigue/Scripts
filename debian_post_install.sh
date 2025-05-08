740c9ed0c76afd239bf5a0da884e28203d37e2f6
Nico <thealdobaldo@gmail.com>
2025-05-08 14:25:43 +0200

    updated script

--- debian_post_install.sh
+++ debian_post_install.sh
@@ -87,3 +87,6 @@ sudo nala install plasma-discover plasma-discover-backend-flatpak
 
 # to make XFCE remember the login at startup:
 # https://libtechnophile.blogspot.com/2024/12/auto-fill-username-in-debian-xfce-login.html
+
+# to configure XFCE whisker menu:
+# https://www.debugpoint.com/xfce-whisker-menu/
