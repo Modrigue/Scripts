38b342409f1dc88a6c7acf0d36669e564a93ad9f
Nico <thealdobaldo@gmail.com>
2025-06-14 17:39:35 +0200

    feat: added commands for XFCE

--- Linux/xfce_commands.sh
+++ Linux/xfce_commands.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+
+# to list all XFCE panels and their plugins:
+for pid in $(xfconf-query -c xfce4-panel -p /panels); do
+    echo "Panel $pid:"
+    xfconf-query -c xfce4-panel -p /panels/panel-$pid/plugin-ids | while read plugin_id; do
+        plugin_type=$(xfconf-query -c xfce4-panel -p /plugins/plugin-$plugin_id)
+        echo "  Plugin $plugin_id → $plugin_type"
+    done
+done
+
+# to move panel X to the top:
+xfconf-query -c xfce4-panel -p /panels/panel-X/position -t string -s "p=10;x=0;y=0"
+
+# to move panel X to the bottom:
+xfconf-query -c xfce4-panel -p /panels/panel-X/position -t string -s "p=10;x=0;y=100"
+
+# to move panel X to the left:
+xfconf-query -c xfce4-panel -p /panels/panel-X/position -t string -s "p=10;x=0;y=0"
+
+# to move panel X to the right:
+xfconf-query -c xfce4-panel -p /panels/panel-X/position -t string -s "p=10;x=100;y=0"
+
+
 # to make XFCE remember the login at startup:
 # https://libtechnophile.blogspot.com/2024/12/auto-fill-username-in-debian-xfce-login.html
 
