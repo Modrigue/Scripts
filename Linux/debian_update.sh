81d848dd63806166d4219bb9dc5ec2a994926c40
Modrigue <thealdobaldo@gmail.com>
2025-07-31 16:50:35 +0200

    refactor: updated script

--- Linux/debian_update.sh
+++ Linux/debian_update.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# My Debian update script
+# Debian update commands
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo flatpak update && sudo apt autoremove -y
 
 # update flatpak packages
 sudo flatpak update
 
 # update sources and packages
 sudo apt update
-sudo apt dist-upgrade -y
+sudo apt upgrade -y
 # or via nala
 sudo apt install --only-upgrade nala -y
 sudo nala upgrade -y
+
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
