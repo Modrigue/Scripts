02baebae78f374282c994320e93045fd863dbc80
Modrigue <thealdobaldo@gmail.com>
2025-08-25 12:44:08 +0200

    feat: updated script

--- Linux/debian_update.sh
+++ Linux/debian_update.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 # Debian update commands
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo apt autoremove -y
-sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo flatpak update && sudo apt autoremove -y
+sudo apt update && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo apt purge '~o' -y
+sudo apt update && sudo apt full-upgrade -y && sudo flatpak update && sudo apt autoremove -y && sudo apt purge '~o' -y
 
 # update flatpak packages
 sudo flatpak update
 
 # update sources and packages
 sudo apt update
-sudo apt upgrade -y
+sudo apt full-upgrade -y
 # or via nala
 sudo apt install --only-upgrade nala -y
-sudo nala upgrade -y
+sudo nala full-upgrade -y
 
-sudo apt dist-upgrade -y
 sudo apt autoremove -y
+sudo apt purge '~o' -y
+sudo apt autoclean -y
